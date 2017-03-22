60134ce5f52e63afcec6fdbdb3bd5bbc448f3425
Jesse Greenberg <jesse.greenberg@colorado.edu>
2017-03-22 13:01:44 -0600

    test ObjectNode in ChargeNode

--- js/common/view/ChargeNode.js
+++ js/common/view/ChargeNode.js
@@ -14,20 +14,15 @@ define( function( require ) {
   var coulombsLaw = require( 'COULOMBS_LAW/coulombsLaw' );
   // var InverseSquareLawCommonConstants = require( 'INVERSE_SQUARE_LAW_COMMON/InverseSquareLawCommonConstants' );
   var inherit = require( 'PHET_CORE/inherit' );
-  // var LinearFunction = require('DOT/LinearFunction');
-  var Node = require( 'SCENERY/nodes/Node' );
-  // var RangeWithValue = require('DOT/RangeWithValue'); 
-  // var ISObjectNode = require('INVERSE_SQUARE_LAW_COMMON/ObjectNode');
+  var PullerPusherNode = require( 'INVERSE_SQUARE_LAW_COMMON/view/PullerPusherNode' );
+  var ISLForceArrowNode = require( 'INVERSE_SQUARE_LAW_COMMON/view/ISLForceArrowNode' );
+  var RangeWithValue = require('DOT/RangeWithValue'); 
+  var RadialGradient = require( 'SCENERY/util/RadialGradient' );
+  var ObjectNode = require('INVERSE_SQUARE_LAW_COMMON/view/ObjectNode');
 
   // strings
-  // TODO: use in Inverse square law (same between Gravity Force and Coulombs Law)
-  // var forceDescriptionPatternTargetSourceString = require( 'string!COULOMBS_LAW/force-description-pattern-target_source' );
-  // var forceDescriptionPatternTargetSourceValueString = require( 'string!COULOMBS_LAW/force-description-pattern-target_source_value' );
-
-  // constants
-  // var PULL_IMAGES_COUNT = 59; // maximum number of images
-  // var forceRange = new RangeWithValue( ( 0.5e-10 ), ( 1.1e-6 ) ); // empirically determined for linear mapping of pull objects
-  // var arrowForceRange = new RangeWithValue( ( 6.0e-9 ), ( 4.1e-6 ) ); // empirically determined for linear mapping of pull objects
+  var charge1String = require( 'string!COULOMBS_LAW/charge1' );
+  var charge2String = require( 'string!COULOMBS_LAW/charge2' );
 
   /**
    * @param {CoulombsLawbModel} model
@@ -39,7 +34,7 @@ define( function( require ) {
    * @constructor
    */
   function ChargeNode( model, chargeObjectModel, layoutBounds, modelViewTransform, tandem, options ) {
-    // var self = this;
+
     options = _.extend( {
       label: 'This Charge',
       otherMassName: 'Other Charge',
@@ -51,18 +46,39 @@ define( function( require ) {
       snapToNearest: null // {number} if present, charge node will snap to the nearest snapToNearest on drag
     }, options );
 
-    // set local variables and constants
+    // @private
+    this.modelViewTransform = modelViewTransform;
 
-    // conversion functions
-    // var forceToArrow = new LinearFunction( arrowForceRange.min, arrowForceRange.max, 1, 60, false );
-    // var forceToArrowMin = new LinearFunction( 0, arrowForceRange.min, 0, 1, false );
-    // var forceToImage = new LinearFunction( pullForceRange.min, pullForceRange.max, 0, PULL_IMAGES_COUNT - 1, true );
+    // @private
+    this.objectModel = chargeObjectModel;
 
-    // ISObjectNode.call( this, stuff );
+    // the puller/pusher robots
+    // functions that determine scaling of the arrow readout and the corrent image to represent
+    var pullForceRange = new RangeWithValue( ( 0.5e-10 ), ( 1.1e-6 ) ); // empirically determined for linear mapping of pull objects
+
+    // the puller/pusher robot    
+    var pullerPusherNode = new PullerPusherNode( pullForceRange, tandem.createTandem( 'puller1' ) );
+
+    // the arrow node, scaled by model ranges and values
+    var arrowForceRange = new RangeWithValue( ( 6.0e-9 ), ( 4.1e-6 ) ); // empirically determined for linear mapping of pull objects
+    var forceArrowNode = new ISLForceArrowNode( charge1String, arrowForceRange, layoutBounds, tandem.createTandem( 'forceArrowNode' ) );
+    
+    ObjectNode.call( this, model, chargeObjectModel, layoutBounds, modelViewTransform, pullerPusherNode, forceArrowNode, tandem.createTandem( 'chargeNode1' ), {
+      label: charge1String,
+      otherObjectName: charge2String
+    } );
   }
 
   coulombsLaw.register( 'ChargeNode', ChargeNode );
 
   // TODO: Inherit from InverseSquareObjectNode
-  return inherit( Node, ChargeNode );
+  return inherit( ObjectNode, ChargeNode, {
+
+    updateGradient: function( baseColor ) {
+      var radius = this.modelViewTransform.modelToViewDeltaX( this.objectModel.radiusProperty.get() );
+      this.objectCircle.fill = new RadialGradient( -radius * 0.6, -radius * 0.6, 1, -radius * 0.6, -radius * 0.6, radius )
+        .addColorStop( 0, baseColor.colorUtilsBrighter( 0.5 ).toCSS() )
+        .addColorStop( 1, baseColor.toCSS() );
+    }
+  } );
 } );
--- js/common/view/CoulombsLawView.js
+++ js/common/view/CoulombsLawView.js
@@ -11,13 +11,16 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var ScreenView = require( 'JOIST/ScreenView' );
   var ResetAllButton = require( 'SCENERY_PHET/buttons/ResetAllButton' );
+  var ChargeNode = require( 'COULOMBS_LAW/common/view/ChargeNode');
+  var ModelViewTransform2 = require( 'PHETCOMMON/view/ModelViewTransform2' );
+  var Vector2 = require( 'DOT/Vector2' );
   var coulombsLaw = require( 'COULOMBS_LAW/coulombsLaw' );
 
   /**
    * @param {CoulombsLawModel} coulombsLawModel
    * @constructor
    */
-  function CoulombsLawView( coulombsLawModel ) {
+  function CoulombsLawView( coulombsLawModel, tandem ) {
 
     ScreenView.call( this );
 
@@ -29,12 +32,24 @@ define( function( require ) {
       right:  this.layoutBounds.maxX - 10,
       bottom: this.layoutBounds.maxY - 10
     } );
+    this.addChild( resetAllButton );
+
+    // Create the model-view transform.  The primary units used in the model are meters, so significant zoom is used.
+    // The multipliers for the 2nd parameter can be used to adjust where the point (0, 0) in the model, which is
+    // between the two masses.
+    var modelViewTransform = ModelViewTransform2.createSinglePointScaleInvertedYMapping(
+      Vector2.ZERO,
+      new Vector2( this.layoutBounds.width / 2, this.layoutBounds.height / 2 ),
+      50
+    );
+
+    var chargeNode = new ChargeNode( coulombsLawModel, coulombsLawModel.object1, this.layoutBounds, modelViewTransform, tandem.createTandem( 'chargeNode1' ) );
+    this.addChild( chargeNode );
 
     // var charge1 = new ChargeNode();
     // var charge2 = new ChargeNode();
     // var ruler = 
 
-    this.setChildren( [resetAllButton] );
   }
 
   coulombsLaw.register( 'CoulombsLawView', CoulombsLawView );
