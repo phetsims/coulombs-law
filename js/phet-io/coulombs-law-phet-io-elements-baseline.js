a9262f429c335e495e84bcddae8953ca2a34074d
zepumph <michael.kauzmann@colorado.edu>
2019-05-29 11:31:58 -0800

    update baseline files, https://github.com/phetsims/phet-io/issues/1466

--- js/phet-io/coulombs-law-phet-io-elements-baseline.js
+++ js/phet-io/coulombs-law-phet-io-elements-baseline.js
@@ -10553,7 +10553,7 @@ window.phet.phetio.phetioElementsBaseline =
     "coulombsLaw.navigationBar.phetButton.pickableProperty": {
       "phetioDocumentation": "Set whether the phetButton will be pickable (and hence interactive), see the NodeIO documentation for more details",
       "phetioEventType": "model",
-      "phetioFeatured": false,
+      "phetioFeatured": true,
       "phetioHighFrequency": false,
       "phetioPlayback": false,
       "phetioReadOnly": false,
