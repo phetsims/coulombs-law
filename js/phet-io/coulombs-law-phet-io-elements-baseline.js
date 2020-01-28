06e9cc5992a15f928ba017f11caf95a0a53c7eb0
zepumph <michael.kauzmann@colorado.edu>
2020-01-27 16:58:24 -0900

    add phetioDocumentation, https://github.com/phetsims/joist/issues/591

--- js/phet-io/coulombs-law-phet-io-elements-baseline.js
+++ js/phet-io/coulombs-law-phet-io-elements-baseline.js
@@ -6476,7 +6476,7 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
     "coulombsLaw.general.soundManager.enabledProperty": {
-      "phetioDocumentation": "",
+      "phetioDocumentation": "If this sim contains sound, then this Property will toggle whether sound is enabled ordisabled.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": false,
