b459e43e8555cbb54c673026c40355704a4d159b
pixelzoom <cmalley@pixelzoom.com>
2020-02-18 20:59:21 -0700

    convert to ES6, https://github.com/phetsims/coulombs-law/issues/116

--- js/common/view/ChargeControl.js
+++ js/common/view/ChargeControl.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2019, University of Colorado Boulder
+// Copyright 2017-2020, University of Colorado Boulder
 
 /**
  * Arrow buttons, slider and text box for editing the object value amount.
@@ -13,7 +13,6 @@ define( require => {
   const ChargeControlSliderThumb = require( 'COULOMBS_LAW/common/view/ChargeControlSliderThumb' );
   const coulombsLaw = require( 'COULOMBS_LAW/coulombsLaw' );
   const Dimension2 = require( 'DOT/Dimension2' );
-  const inherit = require( 'PHET_CORE/inherit' );
   const ISLCObjectControlPanel = require( 'INVERSE_SQUARE_LAW_COMMON/view/ISLCObjectControlPanel' );
   const NumberProperty = require( 'AXON/NumberProperty' );
   const PhetFont = require( 'SCENERY_PHET/PhetFont' );
@@ -24,71 +23,77 @@ define( require => {
   // constants
   const TRACK_SIZE = new Dimension2( 132, 0.25 );
 
-  /**
-   * @param {string} titleString
-   * @param {string} unitString - for the NumberControl readout
-   * @param {Property.<number>} objectProperty - the number Property associated with the ISLCObject
-   * @param {Range} valueRange - max and min values for the object Property, used for display and as NumberControl argument
-   * @param {number} scaleFactor - multiplicative constant for getting proper readouts/positions on Macro and Atomic screens
-   * @param {Object} [options]
-   * @constructor
-   */
-  function ChargeControl( titleString, unitString, objectProperty, valueRange, scaleFactor, options ) {
+  class ChargeControl extends ISLCObjectControlPanel {
 
-    options = merge( {
+    /**
+     * @param {string} titleString
+     * @param {string} unitString - for the NumberControl readout
+     * @param {Property.<number>} objectProperty - the number Property associated with the ISLCObject
+     * @param {Range} valueRange - max and min values for the object Property, used for display and as NumberControl argument
+     * @param {number} scaleFactor - multiplicative constant for getting proper readouts/positions on Macro and Atomic screens
+     * @param {Object} [options]
+     */
+    constructor( titleString, unitString, objectProperty, valueRange, scaleFactor, options ) {
 
-      // panel options
-      align: 'center',
-      additionalTicks: [ { value: 0 } ],
+      options = merge( {
 
-      numberControlOptions: {
-        sliderOptions: {
-          trackSize: TRACK_SIZE,
+        // panel options
+        align: 'center',
+        additionalTicks: [ { value: 0 } ],
 
-          // Instead of having a LinkedProperty to this.chargeControlProperty, link directly to the model Property.
-          phetioLinkedProperty: objectProperty
+        numberControlOptions: {
+          sliderOptions: {
+            trackSize: TRACK_SIZE,
+
+            // Instead of having a LinkedProperty to this.chargeControlProperty, link directly to the model Property.
+            phetioLinkedProperty: objectProperty
+          },
+          titleNodeOptions: { font: new PhetFont( 16 ) },
+          numberDisplayOptions: {
+            font: new PhetFont( 12 ),
+            xMargin: 4,
+            yMargin: 2
+          },
+          arrowButtonOptions: {
+            scale: 0.5
+          }
         },
-        titleNodeOptions: { font: new PhetFont( 16 ) },
-        numberDisplayOptions: {
-          font: new PhetFont( 12 ),
-          xMargin: 4,
-          yMargin: 2
-        },
-        arrowButtonOptions: {
-          scale: 0.5
-        }
-      },
 
-      tandem: Tandem.REQUIRED
-    }, options );
+        tandem: Tandem.REQUIRED
+      }, options );
 
-    // @public {Property.<number>} - intermediate Property to allow for scaling between atomic units and microcoulombs
-    //  - value ranges from -10 to 10
-    //  - unit can be e or mc
-    // TODO: make this DynamicProperty to support PhET-iO LinkedElement, or eliminate altogether
-    this.chargeControlProperty = new NumberProperty( objectProperty.get() * scaleFactor, { range: new Range( -10, 10 ) } );
+      // {Property.<number>} - intermediate Property to allow for scaling between atomic units and microcoulombs
+      //  - value ranges from -10 to 10
+      //  - unit can be e or mc
+      // TODO: make this DynamicProperty to support PhET-iO LinkedElement, or eliminate altogether
+      const chargeControlProperty = new NumberProperty( objectProperty.get() * scaleFactor, {
+        range: new Range( -10, 10 )
+      } );
 
-    // no unlinking/disposing required as Property is never destroyed
-    this.chargeControlProperty.link( function( value ) {
-      objectProperty.set( value / scaleFactor );
-    } );
+      // no unlinking/disposing required as Property is never destroyed
+      chargeControlProperty.link( value => {
+        objectProperty.set( value / scaleFactor );
+      } );
 
-    const chargeControlRange = new Range( valueRange.min * scaleFactor, valueRange.max * scaleFactor );
+      const chargeControlRange = new Range( valueRange.min * scaleFactor, valueRange.max * scaleFactor );
 
-    // add custom thumb to the slider
-    options.numberControlOptions.sliderOptions.thumbNode = new ChargeControlSliderThumb( objectProperty, merge( {}, options,
-      { tandem: options.tandem.createTandem( 'chargeControlsSliderThumb' ) } ) );
+      // add custom thumb to the slider
+      options.numberControlOptions.sliderOptions.thumbNode = new ChargeControlSliderThumb( objectProperty,
+        merge( {}, options, {
+          tandem: options.tandem.createTandem( 'chargeControlsSliderThumb' )
+        } ) );
 
-    ISLCObjectControlPanel.call( this, titleString, unitString, this.chargeControlProperty, chargeControlRange, options );
-  }
+      super( titleString, unitString, chargeControlProperty, chargeControlRange, options );
 
-  coulombsLaw.register( 'ChargeControl', ChargeControl );
-
-  return inherit( ISLCObjectControlPanel, ChargeControl, {
+      // @public
+      this.chargeControlProperty = chargeControlProperty;
+    }
 
     // @public
-    reset: function() {
+    reset() {
       this.chargeControlProperty.reset();
     }
-  } );
+  }
+
+  return coulombsLaw.register( 'ChargeControl', ChargeControl );
 } );
