f8efb596368b832a2e8c564cca2ba4e14131419e
pixelzoom <cmalley@pixelzoom.com>
2020-04-09 16:58:40 -0600

    add options.tandem to ColorProfile, https://github.com/phetsims/joist/issues/625

--- js/common/CoulombsLawColorProfile.js
+++ js/common/CoulombsLawColorProfile.js
@@ -9,6 +9,7 @@
 
 import ColorProfile from '../../../scenery-phet/js/ColorProfile.js';
 import Color from '../../../scenery/js/util/Color.js';
+import Tandem from '../../../tandem/js/Tandem.js';
 import coulombsLaw from '../coulombsLaw.js';
 
 // constants
@@ -45,6 +46,8 @@ const CoulombsLawColorProfile = new ColorProfile( [ 'default', 'projector' ], {
     default: BLACK,
     projector: WHITE
   }
+}, {
+  tandem: Tandem.GLOBAL.createTandem( 'colorProfile' )
 } );
 
 coulombsLaw.register( 'CoulombsLawColorProfile', CoulombsLawColorProfile );
