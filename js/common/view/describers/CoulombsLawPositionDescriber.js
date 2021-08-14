ff489dfdaebbb333a4c4c290f794ade2e22d68fd
Jesse <jesse.greenberg@colorado.edu>
2021-08-14 19:18:21 -0400

    Implement getDistanceIndex in PositionDescriber for all ISLC sims, normalize the distance between objects to describe, see https://github.com/phetsims/gravity-force-lab-basics/issues/206

--- js/common/view/describers/CoulombsLawPositionDescriber.js
+++ js/common/view/describers/CoulombsLawPositionDescriber.js
@@ -14,21 +14,6 @@ class CoulombsLawPositionDescriber extends PositionDescriber {
   constructor( model, object1Label, object2Label ) {
     super( model, object1Label, object2Label );
   }
-
-  /**
-   *
-   * @protected
-   * @param {number} distance
-   * @param {number} numberOfRegions - for crosscheck
-   * @returns {number}
-   * @override
-   */
-  getDistanceIndex( distance, numberOfRegions ) {
-    assert && assert( distance > 0, 'Distance between spheres should always be positive.' );
-
-    // TODO: implement when working on CL descriptions.
-    return 0;
-  }
 }
 
 coulombsLaw.register( 'CoulombsLawPositionDescriber', CoulombsLawPositionDescriber );
