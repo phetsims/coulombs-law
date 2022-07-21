72b3a0adfda7da2b0c0dae66a3854cbda5e07455
phet-dev <phetadmin@gmail.com>
2022-07-21 04:30:52 -0600

    update copyright dates from daily grunt work

--- js/common/CoulombsLawColors.js
+++ js/common/CoulombsLawColors.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2021, University of Colorado Boulder
+// Copyright 2017-2022, University of Colorado Boulder
 
 /**
  * Creates a color profile to handle both a default and projector mode. Mostly allows nodes to be seen with a white
