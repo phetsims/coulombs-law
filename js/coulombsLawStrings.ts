eeed7dada7394a9346ea13a440ddc16b4c2f3395
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:40 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/coulombsLawStrings.ts
+++ js/coulombsLawStrings.ts
@@ -5,51 +5,87 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import coulombsLaw from './coulombsLaw.js';
 
 type StringsType = {
   'charge1': string;
+  'charge1Property': TReadOnlyProperty<string>;
   'charge2': string;
+  'charge2Property': TReadOnlyProperty<string>;
   'charge1Abbreviated': string;
+  'charge1AbbreviatedProperty': TReadOnlyProperty<string>;
   'charge2Abbreviated': string;
+  'charge2AbbreviatedProperty': TReadOnlyProperty<string>;
   'coulombs-law': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'pmScale': string;
+  'pmScaleProperty': TReadOnlyProperty<string>;
   'screen': {
     'macroScale': string;
+    'macroScaleProperty': TReadOnlyProperty<string>;
     'atomicScale': string;
+    'atomicScaleProperty': TReadOnlyProperty<string>;
   };
   'units': {
     'atomicUnits': string;
+    'atomicUnitsProperty': TReadOnlyProperty<string>;
     'atomicLegendScale': string;
+    'atomicLegendScaleProperty': TReadOnlyProperty<string>;
     'macroLegendScale': string;
+    'macroLegendScaleProperty': TReadOnlyProperty<string>;
     'microcoulombs': string;
+    'microcoulombsProperty': TReadOnlyProperty<string>;
   };
   'chargePositionsHeading': string;
+  'chargePositionsHeadingProperty': TReadOnlyProperty<string>;
   'moveChargesLabel': string;
+  'moveChargesLabelProperty': TReadOnlyProperty<string>;
   'moveInLargerStepsLabel': string;
+  'moveInLargerStepsLabelProperty': TReadOnlyProperty<string>;
   'moveInSmallerStepsLabel': string;
+  'moveInSmallerStepsLabelProperty': TReadOnlyProperty<string>;
   'jumpLeftLabel': string;
+  'jumpLeftLabelProperty': TReadOnlyProperty<string>;
   'jumpRightLabel': string;
+  'jumpRightLabelProperty': TReadOnlyProperty<string>;
   'moveRulerHeading': string;
+  'moveRulerHeadingProperty': TReadOnlyProperty<string>;
   'moveRulerLabel': string;
+  'moveRulerLabelProperty': TReadOnlyProperty<string>;
   'chargeAmountHeading': string;
+  'chargeAmountHeadingProperty': TReadOnlyProperty<string>;
   'adjustAmountLabel': string;
+  'adjustAmountLabelProperty': TReadOnlyProperty<string>;
   'adjustInLargerStepsLabel': string;
+  'adjustInLargerStepsLabelProperty': TReadOnlyProperty<string>;
   'jumpToMinimumLabel': string;
+  'jumpToMinimumLabelProperty': TReadOnlyProperty<string>;
   'jumpToMaximumLabel': string;
+  'jumpToMaximumLabelProperty': TReadOnlyProperty<string>;
   'a11y': {
     'moveChargesDescription': string;
+    'moveChargesDescriptionProperty': TReadOnlyProperty<string>;
     'smallStepsMoveChargesDescription': string;
+    'smallStepsMoveChargesDescriptionProperty': TReadOnlyProperty<string>;
     'largeStepsMoveChargesDescription': string;
+    'largeStepsMoveChargesDescriptionProperty': TReadOnlyProperty<string>;
     'jumpHomeDescription': string;
+    'jumpHomeDescriptionProperty': TReadOnlyProperty<string>;
     'jumpEndDescription': string;
+    'jumpEndDescriptionProperty': TReadOnlyProperty<string>;
     'moveRulerDescription': string;
+    'moveRulerDescriptionProperty': TReadOnlyProperty<string>;
     'adjustChargeAmountDescription': string;
+    'adjustChargeAmountDescriptionProperty': TReadOnlyProperty<string>;
     'largeStepsAdjustAmountDescription': string;
+    'largeStepsAdjustAmountDescriptionProperty': TReadOnlyProperty<string>;
     'jumpToMinimumDescription': string;
+    'jumpToMinimumDescriptionProperty': TReadOnlyProperty<string>;
     'jumpToMaximumDescription': string;
+    'jumpToMaximumDescriptionProperty': TReadOnlyProperty<string>;
   }
 };
 
