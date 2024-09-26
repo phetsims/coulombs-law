188c84d1257e64a4001818249ca2690a73cf802f
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:10:12 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for coulombs-law.
+ * ESLint configuration for coulombs-law.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
